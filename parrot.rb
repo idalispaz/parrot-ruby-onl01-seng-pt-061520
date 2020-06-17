7ab92e8a4de1d39c976078fd70bf554fe9e2bb01
Idalis Paz <idalispaz@gmail.com>
2020-06-17 16:51:34 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,13 +1,5 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot (sound = "Squawk!")
-  puts "Squawk!"
-end 
-
-def parrot (phrase = "Squawk!")
-  return phrase
-end
-
 def parrot
-  puts "Squawk!"
+  
 end
